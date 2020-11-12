1131f4c0ba70066d24059119c69e86936fac6821
Andrey Arkhipov <arxipovdev@gmail.com>
2020-11-12 09:54:11 +0400

    add swagger

---
+++ api/api/Options/SwaggerOptions.cs
@@ -0,0 +1,9 @@
+namespace api.Options
+{
+    public class SwaggerOptions
+    {
+        public string Route { get; set; }
+        public string Description { get; set; }
+        public string Endpoint { get; set; }
+    }
+}
--- api/api/Startup.cs
+++ api/api/Startup.cs
@@ -1,47 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using api.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace api
 {
     public class Startup
     {
+        public IConfiguration _configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(op => 
+                op.SwaggerDoc("v1", new OpenApiInfo{ Title = "Api", Version = "v1"} ));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            var swaggerOptions = new SwaggerOptions();
+            _configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+            app.UseSwagger(op => op.RouteTemplate = swaggerOptions.Route);
+            app.UseSwaggerUI(op => 
+                op.SwaggerEndpoint(swaggerOptions.Endpoint, swaggerOptions.Description));
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
